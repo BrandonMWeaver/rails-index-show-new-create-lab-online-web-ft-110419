037f46dfb767c43c8e3852535d1274b5f7f4b4e5
Brandon Weaver <a7xncob@gmail.com>
2020-01-22 19:36:20 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -10,7 +10,8 @@ class CouponsController < ApplicationController
   end
   
   def create
-    coupon = Coupon.create(params)
+    coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to 
   end
   
 end
